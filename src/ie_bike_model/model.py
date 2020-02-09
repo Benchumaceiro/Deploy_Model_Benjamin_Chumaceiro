2563ca43ec995aba6145bb4d98ba3cdce4dd23ce
Benjamin Chumaceiro <benchumaceiro@gmail.com>
2020-02-09 23:32:16 +0100

    Changing model and train_and_persist and adding Ridge

--- src/ie_bike_model/model.py
+++ src/ie_bike_model/model.py
@@ -2,7 +2,7 @@ import datetime as dt
 import os
 from pathlib import Path
 import re
-
+from sklearn.linear_model import Ridge
 import joblib
 import numpy as np
 import pandas as pd
@@ -163,6 +163,25 @@ def train_xgboost(hour):
     xgb.fit(hour_d_train_x, hour_d_train_y)
     return xgb
 
+def train_ridge(hour):
+    # Avoid modifying the original dataset at the cost of RAM
+    hour = hour.copy()
+
+    hour_d = pd.get_dummies(hour)
+    regex = re.compile(r"\[|\]|<", re.IGNORECASE)
+    hour_d.columns = [
+        regex.sub("_", col) if any(x in str(col) for x in set(("[", "]", "<"))) else col
+        for col in hour_d.columns.values
+    ]
+
+    hour_d = hour_d.select_dtypes(exclude="category")
+
+    hour_d_train_x, _, hour_d_train_y, _, = split_train_test(hour_d)
+
+    ridge = Ridge()
+
+    ridge.fit(hour_d_train_x, hour_d_train_y)
+    return ridge
 
 def postprocess(hour):
     # Avoid modifying the original dataset at the cost of RAM
@@ -172,18 +191,17 @@ def postprocess(hour):
     return hour
 
 
-def train_and_persist(model_dir=None, hour_path=None):
+def train_and_persist(model_dir=None, hour_path=None, model='xgboost'):
     hour = read_data(hour_path)
     hour = preprocess(hour)
     hour = dummify(hour)
     hour = postprocess(hour)
-
-    # TODO: Implement other models?
-    model = train_xgboost(hour)
-
-    model_path = get_model_path(model_dir)
-
-    joblib.dump(model, model_path)
+    model_path = get_model_path(model_dir, model)
+    if model == 'xgboost':
+        scikit_model = train_xgboost(hour)  
+    elif model == 'ridge':
+        scikit_model = train_ridge(hour)
+    joblib.dump(scikit_model, model_path)
 
 
 def get_input_dict(parameters):
@@ -228,13 +246,13 @@ def get_input_dict(parameters):
     return df.iloc[0].to_dict()
 
 
-def predict(parameters, model_dir=None):
+def predict(parameters, model_dir=None, model='xgboost'):
     """Returns model prediction.
 
     """
     model_path = get_model_path(model_dir)
     if not os.path.exists(model_path):
-        train_and_persist(model_dir)
+        train_and_persist(model_dir=model_dir, model=model)
 
     model = joblib.load(model_path)
 
